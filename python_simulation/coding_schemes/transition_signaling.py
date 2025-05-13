3ad000f4e231ac289719817caefa43fb36f0ac86
Omri Triki <omritriki@Omris-MacBook-Air.local>
2025-05-13 20:41:43 +0300

    implemented transition signaling

---
+++ python_simulation/coding_schemes/transition_signaling.py
@@ -0,0 +1,61 @@
+"""
+======================================================
+    Power Efficient Error Correction Encoding for
+            On-Chip Interconnection Links
+
+            Shlomit Lenefsky & Omri Triki
+                        06.2025
+======================================================
+"""
+
+from coding_schemes.base_coding_scheme import CodingScheme
+import logging
+
+
+# Description: 
+
+# Inputs:
+
+# Outputs:
+
+
+class Transition_Signaling(CodingScheme):
+    name = "Transition Signaling"
+    supports_errors = False
+
+    # Store previous decoded word
+    def __init__(self):
+        self.s_prev = None  
+    
+
+    def get_bus_size(self, k, M=None) -> int:
+        return k
+
+
+    def encode(self, s_in, c_prev, M=None) -> list[int]:
+        # Initialize with zeros on first decode
+        if self.s_prev is None:
+            self.s_prev = [0] * len(s_in)  
+
+        s_copy = s_in[:] 
+
+        # c = s_in ^ c_prev
+        c = [int(s_copy[i]) ^ int(self.s_prev[i]) for i in range(len(s_copy))]
+
+        logging.debug(f"Transtion Signaling encoded word:       {c}")
+        return c
+    
+
+    def decode(self, c, M=None) -> list[int]:
+        # Initialize with zeros on first decode
+        if self.s_prev is None:
+            self.s_prev = [0] * len(c)  
+            
+        # s_out = c ^ s_prev
+        s_out = [int(c[i]) ^ int(self.s_prev[i]) for i in range(len(c))]
+        
+        # Store current output as previous for next decode
+        self.s_prev = s_out[:]
+        
+        logging.debug(f"Transition Signaling decoded word:      {s_out}")
+        return s_out
