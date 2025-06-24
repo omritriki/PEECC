2a016c31842c6c2b35149740428399e57197b15a
shlomit123 <shlomit.len@gmail.com>
2025-06-24 20:35:30 +0300

    added hamming code (with flexible bits)

--- hamming_exp.py
+++ hamming_exp.py
@@ -42,9 +42,10 @@ def main():
     best_transition_overall = 0  # worst case
 
     f_length = 13
+    info_length = 32
 
     # Generate first codeword
-    full_info1 = ''.join(random.choice('01') for _ in range(32+f_length))
+    full_info1 = ''.join(random.choice('0') for _ in range(info_length+f_length))
     code_word1 = hamming_parity_bits_at_end(full_info1)
 
     for _ in range(50):
@@ -52,11 +53,12 @@ def main():
             print(f"Iteration {_}")
         
         # Generate second info word
-        info_word2 = ''.join(random.choice('01') for _ in range(32))
+        info_word2 = ''.join(random.choice('01') for _ in range(info_length))
         
         # Find optimal flexible part for second codeword
         best_transition_count = 64  # worst case
         cnt_2 = 0
+        cnt_1 = 0
         # Try all possible f_length-bit combinations 
         for i in range(2**f_length):
             f_part2 = format(i, f'0{f_length}b')
@@ -64,22 +66,26 @@ def main():
             code_word2 = hamming_parity_bits_at_end(full_info2)
             
             # Calculate transition count only for redundant bits (f_part + hamming bits)
-            redundant_bits1 = code_word1[32:]
-            redundant_bits2 = code_word2[32:]
+            redundant_bits1 = code_word1[info_length:]
+            redundant_bits2 = code_word2[info_length:]
             transition_count = sum(1 for a, b in zip(redundant_bits1, redundant_bits2) if a != b)
             
             # Keep track of minimum transitions for current codeword
             best_transition_count = min(best_transition_count, transition_count)
+            if transition_count == 1:
+                cnt_1 += 1
 
-            if transition_count == 2:
+            if transition_count <= 2:
                 cnt_2 += 1
                 print("--------------------------------------------------")
-                print(f"Codeword 1: {code_word1[0:32]} | {code_word1[32:45]} | {code_word1[45:]}")
-                print(f"Codeword 2: {code_word2[0:32]} | {code_word2[32:45]} | {code_word2[45:]}")
+                print(f"C_prev: {code_word1[0:info_length]} | {code_word1[info_length:info_length+f_length]} | {code_word1[info_length+f_length:]}")
+                print(f"C_new : {code_word2[0:info_length]} | {code_word2[info_length:info_length+f_length]} | {code_word2[info_length+f_length:]}")
                 #print(f"Info word1: {full_info1[0:32]} | {full_info1[32:]}")
                 #print(f"Info word2: {full_info2[0:32]} | {full_info2[32:]}")
         if cnt_2 > 0:
-            print(f"cnt_2: {cnt_2}")
+            print(f"******* cnt_2: {cnt_2} *******")
+        #if cnt_1 > 0:
+        #    print(f"cnt_1: {cnt_1}")
 
         # Keep track of worst case transition count
         best_transition_overall = max(best_transition_overall, best_transition_count)
